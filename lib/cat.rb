dfbab6aad42ab8059180061131e1293255342c76
contessa <tessa.fallon@gmail.com>
2014-01-15 15:14:31 -0500

    as far as I'm going without internet

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,6 +1,4 @@
 #Has a single model, SpaceCats, with 4 attributes: Name, Home Planet, Tagline, Superpower
 class Cat 
-	def all
-		
-		end
+	attr_accessor :name, :home_planet, :tagline, :superpower
 end
